07a3c5b6f06c2a1c3f2b01ec931393cf6ac7ee50
AndreMP97 <r.quente@hotmail.com>
2018-03-20 18:16:24 +0000

    v0.1

--- .gitignore/my_prompt.c
+++ .gitignore/my_prompt.c
@@ -40,6 +40,7 @@ COMMAND* parse(char *);
 void print_parse(COMMAND *);
 void execute_commands(COMMAND *);
 void free_commlist(COMMAND *);
+void filtro(COMMAND *);
 
 // include do código do parser da linha de comandos
 #include "parser.c"
@@ -87,16 +88,100 @@ void print_parse(COMMAND* commlist) {
 
 
 void free_commlist(COMMAND *commlist){
-    // ...
-    // Esta função deverá libertar a memória alocada para a lista ligada.
-    // ...
+    COMMAND *prev = NULL;
+    while (commlist != NULL){
+      prev = commlist;
+      commlist = commlist -> next;
+      free(prev);
+    }
 }
-
+printf("teste\n");
 
 void execute_commands(COMMAND *commlist) {
-    // ...
-    // Esta função deverá "executar" a "pipeline" de comandos da lista commlist.
-    // ...
-} 
+  pid_t pid;
+  int fd;
+  if (strcmp(commlist -> cmd, "exit") == 0) {
+    exit(0);
+  }
+  else if (strcmp(commlist -> cmd, "filtro") == 0) {
+    filtro(commlist);
+  }
+  if ((pid = fork()) < 0) {
+    printf("***ERROR: fork failed***\n");
+    exit(-1);
+  }
+  else if (pid == 0) {
+    if (inputfile != NULL) {
+      fd = open(inputfile, O_RDONLY);
+      if (fd < 0) {
+        printf("***ERROR! File not found!***\n");
+        exit(-1);
+      }
+      dup2(fd, STDIN_FILENO);
+      close(fd);
+    }
+    if (outputfile != NULL) {
+      int fd = open(outputfile, O_CREAT | O_RDWR, S_IRWXU);
+      dup2(fd, STDOUT_FILENO);
+      close(fd);
+    }
+    if (execvp(commlist -> cmd, commlist -> argv) < 0) {
+      printf("***ERROR: exec failed!***\n");
+      exit(-1);
+    }
+  }
+  else {
+    waitpid(pid,NULL,0);
+  }
+}
 
-
+void filtro(COMMAND *commlist) {
+  int fd[2], file1, file2;
+  pid_t pid1, pid2;
+  if (pipe(fd) < 0) {
+    printf("***ERROR: PIPE FAILED!***\n");
+    exit(-1);
+  }
+  if ((pid1 = fork()) < 0) {
+    printf("***ERROR: FORK CHILD 1 FAILED!***\n");
+    exit(-1);
+  }
+  else if (pid1 == 0) {
+    printf("Debug Filho 1 entrada\n");
+    file1 = open(commlist -> argv[1], O_RDONLY);
+    if (file1 < 0) {
+      printf("***ERROR: FILE NOT FOUND!***\n");
+      exit(-1);
+    }
+    printf("teste 1\n");
+    dup2(file1, STDIN_FILENO);
+    printf("teste 2\n");
+    dup2(fd[PIPE_WRITE], STDOUT_FILENO);
+    printf("teste 3\n");
+    close(fd[PIPE_READ]);
+    printf("teste 4\n");
+    close(fd[PIPE_WRITE]);
+    printf("teste 5\n");
+    execlp("cat", "cat", NULL);
+    printf("Debug filho 1 saída\n");
+  }
+  else {
+    if ((pid2 = fork()) < 0) {
+      printf("***ERROR: FORK CHILD 2 FAILED!***\n");
+      exit(-1);
+    }
+    else if (pid2 == 0) {
+      file2 = open(commlist -> argv[2], O_CREAT | O_TRUNC | O_RDWR, S_IRWXU);
+      dup2(fd[PIPE_READ],STDIN_FILENO);
+      dup2(file2, STDOUT_FILENO);
+      close(fd[PIPE_WRITE]);
+      close(fd[PIPE_READ]);
+      execlp("grep", "grep", commlist -> argv[2], NULL);
+      printf("Debug\n");
+    }
+    else {
+      waitpid(pid1, NULL, 0);
+      waitpid(pid2, NULL, 0);
+    }
+  }
+}
